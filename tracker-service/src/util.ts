d6a1169aac27cc6667cfe44a8ac3e2367a294c56
Sawrr <sawr.harris@gmail.com>
2022-06-29 21:10:15 -0400

    Remove lowercase letters and numbers from room ids

--- tracker-service/src/util.ts
+++ tracker-service/src/util.ts
@@ -13,7 +13,7 @@ export function generateID() {
     const NUM_CHARS = 4;
     
     let id = "";
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     for (let i = 0; i < NUM_CHARS; i++) {
         id += possible.charAt(Math.floor(Math.random() * possible.length));
